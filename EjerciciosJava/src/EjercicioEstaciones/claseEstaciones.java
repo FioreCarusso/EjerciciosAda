ff82a5922a4759aef536c7ec480f4a63f874c6f1
unknown <fiore.carusso@hotmail.com.ar>
2019-11-21 17:28:18 -0300

    subo actualización de la claseEstaciones.java

--- EjerciciosJava/src/EjercicioEstaciones/claseEstaciones.java
+++ EjerciciosJava/src/EjercicioEstaciones/claseEstaciones.java
@@ -3,89 +3,91 @@ package EjercicioEstaciones;
 import java.util.Scanner;
 
 public class claseEstaciones {
-      //creo matriz de 2 arrays
+	// creo matriz de 2 arrays
 	public static void main(String[] args) {
-		float[][] matriz = new float[3][3]; 
-		for (int i = 0; i < matriz.length; i++) { 
-			for (int j = 0; j < matriz.length; j++) { 
-				matriz[i][j] = 0;             
+		int[][] matriz = new int[3][3];
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				matriz[i][j] = 0; // inicializo matriz
 			}
 		}
-        
-		//inicializo variables
-		float totalxNafta[] = new float[3]; 
-		float totalxEstacion[] = new float[3]; 
-		float totalVenta = 0;
-		
-		//comienza el proceso
-		System.out.println("VENTAS DE SURTIDORES"); 
+
+		// inicializo variables
+		int ventasxNafta[] = { 0, 0, 0 }; // nuevos arrays
+		int ventasxEstacion[] = { 0, 0, 0 };
+		int totalVentas = 0;
+		String nombres[] = { "Flores", "Floresta", "Caballito" };
+		String[] naftas = { "Super", "Premium", "GasOil" };
+
+		// comienza el proceso
+		System.out.println("VENTAS DE SURTIDORES");
 		System.out.println("");
-		//pido cantidad de litros
-		System.out.println("Ingrese cantidad de litros");              
+		// pido cantidad de litros
+		System.out.println("Ingrese cantidad de litros");
 		Scanner scan = new Scanner(System.in);
-		float cantLitros = scan.nextFloat();
-		//inicio ciclo while
-		while (cantLitros != 0) {                   
-			//pido tipo de nafta
-			System.out.println("Ingrese tipo de nafta");               
+		int cantLitros = scan.nextInt();
+		// inicio ciclo while
+		while (cantLitros != 0) {
+			// pido tipo de nafta
+			System.out.println("Ingrese tipo de nafta");
 			System.out.println("0) Nafta Super");
 			System.out.println("1) Nafta Premium");
 			System.out.println("2) GasOil");
 			int tipoNafta = scan.nextInt();
-			
-			//calculo venta combustible (litro*valornafta)
-			switch (tipoNafta) {                      
+
+			// calculo venta combustible (litro*valornafta)
+			switch (tipoNafta) {
 			case 0:
-				totalVenta = cantLitros * 20;
+				totalVentas = cantLitros * 20;
 				break;
 			case 1:
-				totalVenta = cantLitros * 40;
+				totalVentas = cantLitros * 40;
 				break;
 			case 2:
-				totalVenta = cantLitros * 10;
+				totalVentas = cantLitros * 10;
 				break;
 
-			}  //pido sucursal
-			System.out.println("Ingrese estación de servicio");        
+			} // pido sucursal
+			System.out.println("Ingrese estación de servicio");
 			System.out.println("0) Shell Flores");
-			System.out.println("1) Shell Caballito");
-			System.out.println("2) Shell Palermo");
+			System.out.println("1) Floresta");
+			System.out.println("2) Caballito");
 			int sucEstacion = scan.nextInt();
-            
-			//calculo ventas totales por tipo de nafta y sucursal  
-			matriz[tipoNafta][sucEstacion] += totalVenta;             
-			totalxNafta[tipoNafta] += totalVenta;                   
-			totalxEstacion[sucEstacion] += totalVenta;
-			
-			//sigo pidiendo valores
-			System.out.println("");
-			System.out.println("Ingrese cantidad de litros");          
-			cantLitros = scan.nextFloat();
 
-		} //termina ciclo while
-		
-		//muestro los resultados 
+			// calculo ventas totales, por tipo de nafta y sucursal
+			matriz[tipoNafta][sucEstacion] += totalVentas;
+			ventasxNafta[tipoNafta] += totalVentas;
+			ventasxEstacion[sucEstacion] += totalVentas;
+
+			// sigo pidiendo valores
+			System.out.println("");
+			System.out.println("Ingrese cantidad de litros");
+			cantLitros = scan.nextInt();
+
+		} // termina ciclo while
+
+		// muestro los resultados
 
 		System.out.println("Total de ganancias por sucursales Shell:");
-		for (int i = 0; i < totalxEstacion.length; i++) {
-			System.out.println(i + ": $" + totalxEstacion[i]);
+		for (int i = 0; i < ventasxEstacion.length; i++) {
+			System.out.println("Sucursal:" + nombres[i] + ": $" + ventasxEstacion[i]);
 		}
 		System.out.println("Total de ganancias por tipo de nafta:");
-		for (int i = 0; i < totalxNafta.length; i++) {
-			System.out.println(i + ": $" + totalxNafta[i]);
+		for (int i = 0; i < ventasxNafta.length; i++) {
+			System.out.println("Nafta" + naftas[i] + ": $" + ventasxNafta[i]);
 		}
 
 		System.out.print("Tipo de nafta más vendida: ");
 		int masVendida = 0;
-		if (totalxNafta[0] > totalxNafta[1] && totalxNafta[0] > totalxNafta[2]) {
-			masVendida = 0;
-		} else if (totalxNafta[1] > totalxNafta[2]) {
-			masVendida = 1;
-		} else {
-			masVendida = 2;
-		}
+		int indicemasVendido = 0;
 
-		System.out.print(masVendida);
+		for (int i = 0; i < ventasxNafta.length; i++) {
+			if (ventasxNafta[i] > masVendida) {
+				masVendida = ventasxNafta[i];
+				indicemasVendido = i;
+			}
+		}
+		System.out.print(naftas[indicemasVendido] + ": " + masVendida);
 
 	}
 
